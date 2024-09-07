9afd0cd17766e2ed6b565da19265f76b7731ff72
codeanythingpossible <contact@codeanythingpossible.com>
2024-09-07 12:05:07 +0200

    FEAT: add some utils for Period

--- src/core/temporality.go
+++ src/core/temporality.go
@@ -11,10 +11,30 @@ type Period struct {
 }
 
 func NewPeriod(start, end time.Time) (*Period, error) {
-	if end.Before(start) {
-		return nil, errors.New("la date de fin doit être après la date de début")
+	if !end.After(start) {
+		return nil, errors.New("end date must be after start date")
 	}
 	return &Period{Start: start, End: end}, nil
+}
+
+// Month returns a Period for the given year and month.
+// The start is the first day of the month, and the end is the first day of the next month (exclusive).
+func Month(year int, month int) (*Period, error) {
+	start := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+
+	nextMonth := start.AddDate(0, 1, 0)
+
+	return NewPeriod(start, nextMonth)
+}
+
+// Year returns a Period for the given year.
+// The start is the first day of the year, and the end is the first day of the next year (exclusive).
+func Year(year int) (*Period, error) {
+	start := time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	nextYear := start.AddDate(1, 0, 0)
+
+	return NewPeriod(start, nextYear)
 }
 
 func (p *Period) Duration() time.Duration {
@@ -23,4 +43,29 @@ func (p *Period) Duration() time.Duration {
 
 func (p *Period) Contains(t time.Time) bool {
 	return (t.After(p.Start) || t.Equal(p.Start)) && (t.Before(p.End) || t.Equal(p.End))
+}
+
+func (p *Period) Split(f func(current time.Time) time.Time) <-chan Period {
+	ch := make(chan Period)
+
+	go func() {
+		defer close(ch)
+
+		current := p.Start
+		for current.Before(p.End) {
+			next := f(current)
+			ch <- Period{Start: current, End: next}
+			current = next
+		}
+	}()
+
+	return ch
+}
+
+func (p *Period) SplitByDays() <-chan Period {
+	return p.Split(func(current time.Time) time.Time { return current.AddDate(0, 0, 1) })
+}
+
+func (p *Period) SplitByMonths() <-chan Period {
+	return p.Split(func(current time.Time) time.Time { return current.AddDate(0, 1, 0) })
 }
--- src/core/temporality_test.go
+++ src/core/temporality_test.go
@@ -50,3 +50,135 @@ func TestPeriod_Duration(t *testing.T) {
 		t.Errorf("expected duration %v, got %v", expectedDuration, period.Duration())
 	}
 }
+
+func TestMonth(t *testing.T) {
+	period, err := Month(2024, 1)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	expectedStart := time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
+	expectedEnd := time.Date(2024, 2, 1, 0, 0, 0, 0, time.UTC)
+
+	if !period.Start.Equal(expectedStart) {
+		t.Errorf("Expected start %v, got %v", expectedStart, period.Start)
+	}
+
+	if !period.End.Equal(expectedEnd) {
+		t.Errorf("Expected end %v, got %v", expectedEnd, period.End)
+	}
+}
+
+func TestYear(t *testing.T) {
+	period, err := Year(2024)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	expectedStart := time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
+	expectedEnd := time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	if !period.Start.Equal(expectedStart) {
+		t.Errorf("Expected start %v, got %v", expectedStart, period.Start)
+	}
+
+	if !period.End.Equal(expectedEnd) {
+		t.Errorf("Expected end %v, got %v", expectedEnd, period.End)
+	}
+}
+
+func TestPeriod_SplitJanuaryByDays(t *testing.T) {
+	period, err := Month(2024, 1)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	days := period.SplitByDays()
+	count := 0
+	current := *period
+
+	for d := range days {
+		count++
+		current = d
+	}
+
+	if !current.Start.Equal(time.Date(2024, 1, 31, 0, 0, 0, 0, time.UTC)) {
+		t.Errorf("expected start %v, got %v", time.Date(2024, 1, 31, 0, 0, 0, 0, time.UTC), current.Start)
+	}
+
+	if count != 31 {
+		t.Errorf("expected 31 days, got %v", count)
+	}
+}
+
+func TestPeriod_SplitFebruaryByDays(t *testing.T) {
+	period, err := Month(2024, 2)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	days := period.SplitByDays()
+	count := 0
+	current := *period
+
+	for d := range days {
+		count++
+		current = d
+	}
+
+	if !current.Start.Equal(time.Date(2024, 2, 29, 0, 0, 0, 0, time.UTC)) {
+		t.Errorf("expected start %v, got %v", time.Date(2024, 2, 29, 0, 0, 0, 0, time.UTC), current.Start)
+	}
+
+	if count != 29 {
+		t.Errorf("expected 29 days, got %v", count)
+	}
+}
+
+func TestPeriod_SplitYear2024ByDays(t *testing.T) {
+	period, err := Year(2024)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	days := period.SplitByDays()
+	count := 0
+	current := *period
+
+	for d := range days {
+		count++
+		current = d
+	}
+
+	if !current.Start.Equal(time.Date(2024, 12, 31, 0, 0, 0, 0, time.UTC)) {
+		t.Errorf("expected start %v, got %v", time.Date(2024, 12, 31, 0, 0, 0, 0, time.UTC), current.Start)
+	}
+
+	if count != 366 {
+		t.Errorf("expected 31 days, got %v", count)
+	}
+}
+
+func TestPeriod_SplitYear2024ByMonths(t *testing.T) {
+	period, err := Year(2024)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	days := period.SplitByMonths()
+	count := 0
+	var current Period
+
+	for d := range days {
+		count++
+		current = d
+	}
+
+	if !current.Start.Equal(time.Date(2024, 12, 1, 0, 0, 0, 0, time.UTC)) {
+		t.Errorf("expected start %v, got %v", time.Date(2024, 12, 1, 0, 0, 0, 0, time.UTC), current.Start)
+	}
+
+	if !current.End.Equal(time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC)) {
+		t.Errorf("expected end %v, got %v", time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), current.End)
+	}
+
+	if count != 12 {
+		t.Errorf("expected 12 months, got %v", count)
+	}
+}
