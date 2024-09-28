17da9b14b56c6852f531c892ff250e130237db7a
codeanythingpossible <contact@codeanythingpossible.com>
2024-09-28 18:51:56 +0200

    chore: using GoTimelines package

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
-module src
+module GoNextFund
 
-go 1.23
+go 1.23.1
+
+require github.com/codeanythingpossible/GoTimelines v0.0.2-alpha // indirect
--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/codeanythingpossible/GoTimelines/timelines"
 	"html/template"
 	"net/http"
 )
@@ -11,9 +12,42 @@ type Data struct {
 }
 
 func main() {
+	timeline1, err := timelines.
+		NewTimeLineBuilder[int]().
+		AddMonth(2024, 1, 100).
+		AddMonth(2024, 2, 200).
+		AddMonth(2024, 3, 300).
+		AddPeriodValue(timelines.PeriodValue[int]{
+			Period: timelines.Period{
+				Start: timelines.DateOnly(2024, 1, 10),
+				End:   timelines.DateOnly(2024, 1, 17),
+			},
+			Value: 80,
+		}).
+		AddPeriodValue(timelines.PeriodValue[int]{
+			Period: timelines.Period{
+				Start: timelines.DateOnly(2024, 1, 12),
+				End:   timelines.DateOnly(2024, 1, 15),
+			},
+			Value: 50,
+		}).Build()
+
+	if err != nil {
+		fmt.Println("Erreur lors de la construction de la timeline :", err)
+		return
+	}
+
+	timeline1, err = timeline1.ResolveConflicts(func(p timelines.Period, a int, b int) int {
+		return a + b
+	})
+
+	for _, pv := range timeline1.Items {
+		fmt.Println(pv)
+	}
+
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/update", updateHandler)
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
